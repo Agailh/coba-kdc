906df8c71ea74d61d79b0a146a6d86b91a3d77be
Aga Ilham <agailham86@gmail.com>
2023-11-28 17:12:48 +0700

    bug on search

--- app/Controllers/Performance.php
+++ app/Controllers/Performance.php
@@ -20,30 +20,46 @@ class Performance extends BaseController
 
     public function index()
     {
-
-        // $kdcModel = new kdcModel();
-        // $kdcModel->findAll();
-        // dd($kdcModel);
-        // $picModel = $this->picModel->findAll();
-        // $kdcModel = $this->kdcModel->findAll();
         $keyword = $this->request->getVar('keyword');
 
-        // Search in picModel
-        // if ($keyword) {
-        //     $data['pic'] = $this->picModel->search($keyword);
-        // } else {
-        //     $data['pic'] = $this->picModel->getAll();
-        // }
+        if ($keyword) {
+            $data['pic'] = $this->picModel->search($keyword);
+        } else {
+            $data['pic'] = $this->picModel->getAll();
+        }
 
-        // $data = [
-
-        //     // 'pic' => $picModel,
-        //     // 'kdc' => $kdcModel,
-
-        // ];
-        // $data['kdc'] = $this->kdcModel->getAll();
-        $data['pic'] = $this->picModel->getAll();
 
         return view('/Performance/index', $data);
     }
 }
+
+
+
+// public function index()
+// {
+
+//     // $kdcModel = new kdcModel();
+//     // $kdcModel->findAll();
+//     // dd($kdcModel);
+//     // $picModel = $this->picModel->findAll();
+//     // $kdcModel = $this->kdcModel->findAll();
+//     $keyword = $this->request->getVar('keyword');
+
+//     // Search in picModel
+//     // if ($keyword) {
+//     //     $data['pic'] = $this->picModel->search($keyword);
+//     // } else {
+//     //     $data['pic'] = $this->picModel->getAll();
+//     // }
+
+//     // $data = [
+
+//     //     // 'pic' => $picModel,
+//     //     // 'kdc' => $kdcModel,
+
+//     // ];
+//     // $data['kdc'] = $this->kdcModel->getAll();
+//     $data['pic'] = $this->picModel->getAll();
+
+//     return view('/Performance/index', $data);
+// }
