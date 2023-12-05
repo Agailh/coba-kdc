f1ac4b612a983dccb55f08e00e2ca2b68e850e2a
Aga Ilham <agailham86@gmail.com>
2023-12-05 17:31:07 +0700

    change email confrimation header

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -6,8 +6,8 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = 'agailham327@gmail.com';
-    public string $fromName   = 'Agailh';
+    public string $fromEmail  = 'arrow12537440aga@gmail.com';
+    public string $fromName   = 'KDC APP ADMIN';
     public string $recipients = '';
 
     /**
